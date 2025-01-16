3809c1782e2c921730af6aa1fc5cefbb061838f9
Yago Guirra <yagoguirra@hotmail.com>
2025-01-16 12:59:29 -0300

    first commit

---
+++ inc/buzzer.h
@@ -0,0 +1,10 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+// Macros
+
+#define BUZZER_A_PIN 21
+
+// Cabeçalhos
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "pico/stdlib.h"
+#include "inc/buzzer.h"
+#include "inc/led.h"
+
+int main() {
+
+}
