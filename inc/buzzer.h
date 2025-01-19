8c21a3ca6f4008fdc9094411e7d1ce3f6402c6bd
Camila-Miranda <camilasilveirabm@gmail.com>
2025-01-19 12:33:10 -0300

    Alteração das funçoes

--- inc/buzzer.h
+++ inc/buzzer.h
@@ -7,4 +7,16 @@
 
 // Cabeçalhos
 
+// Funçao para inicializa o Buzzer
+void buzzer_init(uint gpio);
+
+// Funçao para liga o Buzzer em uma determinada frequência
+void buzzer_turn_on(uint gpio, uint freq);
+
+// Funçao para desliga o Buzzer
+void buzzer_turn_off(uint gpio);
+
+// Funçao para ativa o Buzzer por um tempo determinado (em milissegundos)
+void buzzer_beep(uint gpio, uint freq, uint duration_ms);
+
 #endif
